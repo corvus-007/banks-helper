0f1df098c2b5a118da7f651b134c87d0eaf487dd
denis.vasiliev <denis.vasilev@royalpay.eu>
2024-03-29 10:41:34 +0300

    update index.ts

--- index.ts
+++ index.ts
@@ -3,25 +3,33 @@ import { IBankLogoItem } from './IBankLogoItem';
 
 const UNKNOWN_BANK_FILE_NAME = 'unknown.svg';
 
-const findBank = (bankName: string): IBankLogoItem => {
-  return banks.find(({ matchedBankNames, humanName }) => {
-    return [...matchedBankNames, humanName].map(it => it.toLowerCase()).includes(bankName.toLowerCase());
+const findBank = (bankName: string, banksList: IBankLogoItem[] = banks): IBankLogoItem => {
+  if (!bankName) {
+    return;
+  }
+
+  const lowerBankName = bankName.toLowerCase();
+
+  return banksList.find(({ matchedBankNames, humanName }) => {
+    return Array.from([...matchedBankNames, humanName], it => it.toLowerCase())
+                .some(it => it.includes(lowerBankName));
   });
 };
 
-const getBankLogoFileName = (bankName: string) => {
-  const bankLogoItem = findBank(bankName);
+const getBankLogoFileName = (bankName: string, banksList: IBankLogoItem[] = banks) => {
+  const bankLogoItem = findBank(bankName, banksList);
 
   return bankLogoItem?.fileName || UNKNOWN_BANK_FILE_NAME;
 };
 
-const getBankLogoName = (bankName: string): string => {
-  const bankLogoItem = findBank(bankName);
+const getBankLogoName = (bankName: string, banksList: IBankLogoItem[] = banks): string => {
+  const bankLogoItem = findBank(bankName, banksList);
 
   return bankLogoItem?.humanName || bankName;
 };
 
 export {
+  UNKNOWN_BANK_FILE_NAME,
   getBankLogoFileName,
   getBankLogoName,
 };
